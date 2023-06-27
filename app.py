a6c2cbe0ceee726d4fa295d41adedf53b9da26d6
steve2700 <stevnyaruwa@gmail.com>
2023-06-27 20:50:30 +0200

    updating the notifications of my flask todo app

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from huey import RedisHuey
 from flask_migrate import Migrate
-from datetime import datetime, timedelta
-import os
+from datetime import datetime
+from huey import RedisHuey
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+# Configure Redis for Huey task queue
+huey = RedisHuey(url='redis://localhost:6379/0')
 
 
 class Todo(db.Model):
@@ -16,11 +19,19 @@ class Todo(db.Model):
     content = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Integer, default=0)
     pub_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    category = db.Column(db.String(50), nullable=False)
+    category = db.Column(db.String(50), nullable=False, unique=True)
     notes = db.Column(db.Text)
 
     def __repr__(self):
         return '<Task %r>' % self.id
+
+
+@huey.task()
+def send_reminder(task_id):
+    # Logic for sending reminder/notification
+    task = Todo.query.get(task_id)
+    if task:
+        print(f"Sending reminder for task: {task.content}")
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -32,9 +43,14 @@ def index():
         try:
             db.session.add(new_task)
             db.session.commit()
+
+            # Queue reminder task
+            send_reminder(new_task.id)
+
             return redirect('/')
-        except:
-            return 'There was an issue adding your task.'
+        except SQLAlchemyError as e:
+            error_msg = str(e.__dict__.get('orig'))
+            return f'There was an issue adding your task: {error_msg}'
     else:
         tasks = Todo.query.order_by(Todo.pub_date).all()
         return render_template('index.html', tasks=tasks)
@@ -47,8 +63,9 @@ def delete(id):
         db.session.delete(task_to_delete)
         db.session.commit()
         return redirect('/')
-    except:
-        return 'There was a problem deleting that task.'
+    except SQLAlchemyError as e:
+        error_msg = str(e.__dict__.get('orig'))
+        return f'There was a problem deleting that task: {error_msg}'
 
 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
@@ -60,11 +77,11 @@ def update(id):
         try:
             db.session.commit()
             return redirect('/')
-        except:
-            return 'There was an issue updating your task.'
+        except SQLAlchemyError as e:
+            error_msg = str(e.__dict__.get('orig'))
+            return f'There was an issue updating your task: {error_msg}'
     else:
         return render_template('update.html', task=task)
-
 
 
 if __name__ == '__main__':
