f345543f4f352023043fb12a271635ae26d9a968
steve2700 <stevnyaruwa@gmail.com>
2023-06-30 21:09:03 +0200

    delete huey now working with the flask mail what a journey

--- app.py
+++ app.py
@@ -1,27 +1,35 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from huey import RedisHuey
-import redis
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
-# Create a Redis client instance
-redis_client = redis.Redis(host='localhost', port=6379)
+# Configure Flask-Mail
+app.config['MAIL_SERVER'] = 'smtp.example.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'taskbuddy27@gmail.com'
+app.config['MAIL_PASSWORD'] = 'gogochuchu27'
+app.config['MAIL_DEFAULT_SENDER'] = 'taskbuddy27@gmail.com'
+
+db = SQLAlchemy(app)
+mail = Mail(app)
+
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Integer, default=0)
     pub_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    category = db.Column(db.String(50), nullable=False, unique=True)
+    category = db.Column(db.String(50), nullable=False)
     notes = db.Column(db.Text)
 
     def __repr__(self):
         return '<Task %r>' % self.id
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -33,19 +41,16 @@ def index():
             db.session.add(new_task)
             db.session.commit()
 
-            # Add the task content to Redis set for notifications
-            redis_client.sadd('task_notifications', new_task.content)
+            # Send email notification
+            send_notification_email(new_task)
 
             return redirect('/')
         except Exception as e:
             return f'There was an issue adding your task: {str(e)}'
     else:
         tasks = Todo.query.order_by(Todo.pub_date).all()
+        return render_template('index.html', tasks=tasks)
 
-        # Get task notifications from Redis set
-        task_notifications = redis_client.smembers('task_notifications')
-
-        return render_template('index.html', tasks=tasks, notifications=task_notifications)
 
 @app.route('/delete/<int:id>')
 def delete(id):
@@ -53,13 +58,20 @@ def delete(id):
     try:
         db.session.delete(task_to_delete)
         db.session.commit()
-
-        # Remove the task content from Redis set for notifications
-        redis_client.srem('task_notifications', task_to_delete.content)
-
         return redirect('/')
     except Exception as e:
         return f'There was a problem deleting that task: {str(e)}'
+
+
+def send_notification_email(task):
+    subject = 'New Task Added'
+    recipient = 'user@example.com'  # Replace with the recipient's email address
+    body = f'A new task has been added please dont forget to look out: {task.content}'
+
+    with app.app_context():
+        msg = Message(subject=subject, recipients=[recipient], body=body)
+        mail.send(msg)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- huey_queue.py
+++
@@ -1,6 +0,0 @@
-from huey import RedisHuey
-from app import huey
-
-huey_consumer = huey.consumer()
-huey_consumer.run()
-
