8a08d6d4a9634916e06b90cbe352ec331049ffe6
steve2700 <stevnyaruwa@gmail.com>
2023-07-01 00:45:47 +0200

    app.py

--- app.py
+++ app.py
@@ -1,22 +1,37 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_mail import Mail, Message
+from flask_login import LoginManager, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = '6f8673a8f3b9452dc5f08f8158f82db4'
 
 # Configure Flask-Mail
-app.config['MAIL_SERVER'] = 'smtp.example.com'
+app.config['MAIL_SERVER'] = 'smtp.google.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'taskbuddy27@gmail.com'
+app.config['MAIL_USERNAME'] = 'taskbuddy27@google.com'
 app.config['MAIL_PASSWORD'] = 'gogochuchu27'
-app.config['MAIL_DEFAULT_SENDER'] = 'taskbuddy27@gmail.com'
+app.config['MAIL_DEFAULT_SENDER'] = 'taskbuddy27@google.com'
 
 db = SQLAlchemy(app)
 mail = Mail(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Todo(db.Model):
@@ -24,8 +39,8 @@ class Todo(db.Model):
     content = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Integer, default=0)
     pub_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    category = db.Column(db.String(50), nullable=False)
-    notes = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', backref=db.backref('todos', lazy=True))
 
     def __repr__(self):
         return '<Task %r>' % self.id
@@ -33,10 +48,12 @@ class Todo(db.Model):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    if not current_user.is_authenticated:
+        return redirect('/login')
+
     if request.method == 'POST':
         task_content = request.form['task']
-        task_category = request.form['category']
-        new_task = Todo(content=task_content, category=task_category)
+        new_task = Todo(content=task_content, user=current_user)
         try:
             db.session.add(new_task)
             db.session.commit()
@@ -48,7 +65,7 @@ def index():
         except Exception as e:
             return f'There was an issue adding your task: {str(e)}'
     else:
-        tasks = Todo.query.order_by(Todo.pub_date).all()
+        tasks = Todo.query.filter_by(user=current_user).order_by(Todo.pub_date).all()
         return render_template('index.html', tasks=tasks)
 
 
@@ -63,10 +80,59 @@ def delete(id):
         return f'There was a problem deleting that task: {str(e)}'
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user:
+            flash('Email already exists. Please login.')
+            return redirect('/login')
+
+        new_user = User(email=email)
+        new_user.set_password(password)
+
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Registration successful. Please login.')
+            return redirect('/login')
+        except Exception as e:
+            return f'There was an issue registering the user: {str(e)}'
+    else:
+        return render_template('register.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        user = User.query.filter_by(email=email).first()
+        if not user or not user.check_password(password):
+            flash('Invalid email or password.')
+            return redirect('/login')
+
+        # User authentication successful
+        login_user(user)
+        return redirect('/')
+    else:
+        return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect('/login')
+
+
 def send_notification_email(task):
     subject = 'New Task Added'
-    recipient = 'user@example.com'  # Replace with the recipient's email address
-    body = f'A new task has been added please dont forget to look out: {task.content}'
+    recipient = current_user.email
+    body = f'A new task has been added: {task.content}'
 
     with app.app_context():
         msg = Message(subject=subject, recipients=[recipient], body=body)
@@ -74,5 +140,5 @@ def send_notification_email(task):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
 
